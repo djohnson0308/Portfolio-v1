a8f32a00541aa6e255676d3bf2e2523bf41225d3
Devon Johnson <dljohnson@rollins.edu>
2017-03-16 00:04:04 -0400

    scroll removed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,14 +10,16 @@ TweenMax.to($img, 2.5, {autoAlpha:1});
 
 TweenMax.to(".logo", 2.5, {left:50, top: 30});
 
-TweenMax.to (".hsContent", 2.5, {bottom: 5, left: 700});
+TweenMax.to (".hsContent", 2.5, {});
+// left: 700px;
+// bottom: 50, left: 520
 
 TweenMax.set(".footer-logo", {opacity:0});
 TweenMax.to(".footer-logo", 2, { y:-160 , opacity:1 , ease:Power1.easeInOut});
 
 var snow = $(".hsContent");
 var tl = new TimelineMax({repeat:-1});
-var bezierPath = [{ x: 8, y: 7 }, { x: -5, y: 8 }, { x: 5, y: 12 },{ x: -5, y: 16 }, {x: 0, y: 0}];
+var bezierPath = [{ x: 3, y: 7 }, { x: -5, y: 8 }, { x: 5, y: 12 },{ x: -5, y: 16 }, {x: 0, y: 0}];
 
 // tl.set(snow, {autoAlpha:0});
 tl.to(snow, 20, { bezier: {
